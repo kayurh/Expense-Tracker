c5bc4ab298dc09c1d541e81a670f4fc190bac53e
kayurh <kayurharry2025@gmail.com>
2025-03-27 22:22:43 +0100

    Arrays to store user inputs
    There are 4 arrays that each store a value and a category for an income or expense respectively

---
+++ Storage.java
@@ -0,0 +1,8 @@
+int[] incomeValue = new int[incomes];
+int[] expenseValue = new int[expenses];
+
+String[] incomeType = new String[incomes];
+String[] expenseType = new String[expenses];
+
+int totalIncome = 0;
+int totalExpense = 0;
