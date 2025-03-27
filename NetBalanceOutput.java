105a37293a49939d28f8f25f87a58a543383a5a8
kayurh <kayurharry2025@gmail.com>
2025-03-27 22:29:05 +0100

    The net balance of the user inputs is printed
    The code calculates the net balance and returns an appropriate statement (Whether surplus or deficit)

---
+++ NetBalanceOutput.java
@@ -0,0 +1,13 @@
+int netBalance = totalIncome - totalExpense;
+
+if(netBalance > 0)
+{
+    System.out.println("You have a surplus");
+}
+
+else
+{
+    System.out.println("You have a deficit");
+}
+
+in.close();
